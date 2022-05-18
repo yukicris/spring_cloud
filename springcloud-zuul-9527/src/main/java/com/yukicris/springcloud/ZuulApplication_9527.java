a80e7f93c2c388be8073a1bcf958500e9900ac7e
yukicris <yewei2008com@yeah.net>
2022-05-18 18:12:12 +0800

    zuul路由网关 springconfig远程配置 3344+3355

---
+++ springcloud-zuul-9527/src/main/java/com/yukicris/springcloud/ZuulApplication_9527.java
@@ -0,0 +1,14 @@
+package com.yukicris.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy  //使用代理
+public class ZuulApplication_9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication_9527.class,args);
+    }
+}
